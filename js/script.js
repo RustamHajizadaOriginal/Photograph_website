b2eda580ddbf70f56a99eaa65e8ea9e986840726
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-02-14 14:32:59 +0100

    Background Pictures

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function
+    () {
+    // Mobile Menu
+    const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+    
+    toggleButton.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active');
+    });
+});
